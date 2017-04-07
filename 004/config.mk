15898d9ce004a5a7f6ff6a3c59e5ab1de947bb47
FRPV <frpvillatoro@gmail.com>
2017-04-07 12:05:08 -0500

    Adding step for joint genotyping
    In this step all gVCFs are genotyping together

---
+++ 004/config.mk
@@ -0,0 +1,14 @@
+#Same castillo PATH
+PATH=/castle/cfresno/.bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin:/usr/lib/plan9/bin:/usr/bin/
+
+#GATK directory
+GATK="/usr/share/java/GenomeAnalysisTK3.7.jar"
+
+#Directorio del genoma de referencia
+REF="/100g/references/human-reference/v37_decoy/human_g1k_v37_decoy.fasta"
+
+#Directorio con variantes de confianza
+VARIANTS="/100g/references/reference_variants-gatk/1000G_phase1.indels.b37.vcf"
+
+#Number of threads
+NT="16"
