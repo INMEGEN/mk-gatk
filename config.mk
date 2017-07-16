6ab926934357936c9fa2057e9d14cc8b1080f5b8
Iaguilaror <iaguilaror@gmail.com>
2017-07-16 00:48:32 -0500

    added $HAPMAP, $1000G_OMNI. MUST add correct paths.

--- config.mk
+++ config.mk
@@ -10,6 +10,8 @@ REF="/reference/ftp.broadinstitute.org/bundle/hg38/Homo_sapiens_assembly38.fasta
 #Directorio con variantes de confianza
 INDELs="/reference/ftp.broadinstitute.org/bundle/hg38/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz"
 dbSNP="/reference/ftp.broadinstitute.org/bundle/hg38/dbsnp_138.hg38.vcf.gz"
+HAPMAP= ##originally "REF/1000G_omni2.5.b37.vcf" in 005; must find correct path in /reference @cluster.inmegen.gob.mx
+1000G_OMNI= ##originally "REF/1000G_omni2.5.b37.vcf" in 005; must find correc path in /reference @cluster.inmegen.gob.mx
 
 #Number of threads
 NT="4"
